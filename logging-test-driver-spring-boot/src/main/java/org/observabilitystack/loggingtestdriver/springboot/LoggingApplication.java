e61b2471c0a99d58e0218297df86716a825da82b
Nikolaus Winter <nikolaus.winter@s24.com>
2019-01-26 22:45:23 +0100

    Logging frequency can be set for Spring Boot test driver

--- logging-test-driver-spring-boot/src/main/java/org/observabilitystack/loggingtestdriver/springboot/LoggingApplication.java
+++ logging-test-driver-spring-boot/src/main/java/org/observabilitystack/loggingtestdriver/springboot/LoggingApplication.java
@@ -1,39 +1,49 @@
 package org.observabilitystack.loggingtestdriver.springboot;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class LoggingApplication {
+
+    private static final int DEFAULT_LOG_LINES_PER_MINUTE = 60;
+
+    private final static String ENV_LOG_LINES_PER_MINUTE = "LOG_LINES_PER_MINUTE";
 
     public static void main(String[] args) {
         SpringApplication.run(LoggingApplication.class, args);
     }
 
     @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+    public Runnable sleeper(Environment env) {
+        final Integer logLinesPerMinute = env.getProperty(ENV_LOG_LINES_PER_MINUTE, Integer.class, DEFAULT_LOG_LINES_PER_MINUTE);
+        return () -> {
+            try {
+                Thread.sleep((long) (240_000 * Math.random() / logLinesPerMinute));
+            } catch (Exception e) {
+                // It is okay to ignore this one ...
+            }
+        };
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx, Runnable sleeper) {
         return args -> {
             new Thread(() -> {
                 while (true) {
-                    try {
-                        Thread.sleep(500);
-                        LoggingClass.INSTANCE.log();
-                    } catch (InterruptedException e) {
-                        // It is okay to ignore this one ...
-                    }
+                    sleeper.run();
+                    LoggingClass.INSTANCE.log();
                 }
             }).start(); 
             new Thread(() -> {
                 while (true) {
-                    try {
-                        Thread.sleep(500);
-                        LoggingClass.INSTANCE.log();
-                    } catch (InterruptedException e) {
-                        // It is okay to ignore this one ...
-                    }
+                    sleeper.run();
+                    LoggingClass.INSTANCE.log();
                 }
             }).start(); 
         };
